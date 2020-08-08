661330226e265b708b594203f8746e6bf0bea161
sakura <66314283+Sakura-rua@users.noreply.github.com>
2020-08-09 02:06:57 +0800

    Update tg_username_update.py

--- tg_username_update.py
+++ tg_username_update.py
@@ -56,13 +56,14 @@ async def change_name_auto():
                 hsym = time_emoji_symb[(int(hour)%12)*2+shift]
                 # await client1.send_message('me', hsym)
                 for_fun = random.random() 
-                if for_fun < 0.10:
-                    last_name = '%s时%s分 %s' % (hour, minu, hsym)
-                elif for_fun < 0.30:
-                    last_name = '%s:%s %s %s %s' % (hour, minu, p, abbwn, hsym)
-                elif for_fun < 0.60:
+                #if for_fun < 0.10:
+                #    last_name = '%s时%s分 %s' % (hour, minu, hsym)
+                #elif for_fun < 0.30:
+                #    last_name = '%s:%s %s %s %s' % (hour, minu, p, abbwn, hsym)
+                #elif for_fun < 0.60:
                     last_name = '%s:%s %s UTC+8 %s' % (hour, minu, p, hsym)
-                elif for_fun < 0.90:
+                #elif for_fun < 0.90:
+                if for_fun < 0.90:
                     last_name = '%s' % dizzy
                 else:
                     last_name = '%s' % cake
